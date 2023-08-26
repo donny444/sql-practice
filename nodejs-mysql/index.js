4fa786c0656ebd65fecfb2b05f0f6dbc7548a6fb
donny_444 <104888835+donny444@users.noreply.github.com>
2023-08-26 23:00:20 +0700

    Add files via upload

---
+++ nodejs-mysql/index.js
@@ -0,0 +1,46 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+});
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+//C
+var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+con.query(sql, function (err) {
+    if (err) throw err;
+    console.log("Table created");
+});
+
+//R
+var sql = "SELECT * FROM customers";
+con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+});
+
+//U
+var sql = "UPDATE customers SET address = 'Highway 44' WHERE address = 'Highway 37'";
+con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+});
+
+//D
+var sql = "DELETE FROM customers WHERE name = 'Company Inc'";
+con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+});
+
+con.end();
+
+//Source: https://www.w3schools.com/nodejs/nodejs_mysql.asp
